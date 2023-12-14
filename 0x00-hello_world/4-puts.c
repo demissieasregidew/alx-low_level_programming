1e0cb8c90c99779608f3c7dbcc0c400d75f5a3f0
demissieasregidew <demissieasregidew@gmail.com>
2023-12-14 13:52:19 -0800

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,3 @@
-
 #include <stdio.h>
 /**
  * main - Entry point
@@ -7,6 +6,6 @@
  */
 int main (void)
 {
-	putss("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return(0);
 }
