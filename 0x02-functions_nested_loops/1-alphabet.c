4a9e0b15272df02931e205376b12718759944231
demissieasregidew <demissieasregidew@gmail.com>
2023-12-20 14:46:59 -0800

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Entry point
+ * Return:0
+ */
+
+void print_alphabet(void)
+{
+	char ch;
+for (ch = 'a' ch <= 'x'; ch++)
+_putchar(ch);
+putchar('\n');
+
+}
