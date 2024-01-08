22c7eef56e1bfc53c24b5111aadf0cd71effc757
demissieasregidew <demissieasregidew@gmail.com>
2024-01-08 12:47:58 -0800

    print _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,17 @@
 #include "main.h"
+
 /**
- * main - Entry Block
-  *Description: prints _putchar followed by a new line
- * Return: 0
+ *print_numbers - a function that prints the numbers, from 0 to 9
+ *
+ *Return: 0-9 followed by newline
+ * Example result :
+ * 0,1,2,3,4,5,6,7,8,9
  */
-int main(void)
+void print_numbers(void)
 {
-	char c[8] = "_putchar";
+	int i;
 
-	int i = 0;
-
-	while (i < 8)
-	{
-		_putchar(c[i]);
-		i++;
-	}
+	for (i = 0; i <= 9; i++)
+		_putchar(i + '0');
 	_putchar('\n');
-	return (0);
 }
